5530721f7b00db371687a9289a5b660f39bd6b66
Lai99 <aaaxaaa99@gmail.com>
2015-10-27 02:05:31 +0800

    add range search

--- post/data_post.py
+++ post/data_post.py
@@ -6,28 +6,42 @@
 #
 # Created:     26/10/2015
 #-------------------------------------------------------------------------------
+from xlwings import Workbook, Sheet, Range, Chart
+
+def seq_to_int(string):
+    pos = -1
+    sum_str = 0
+    for i in range(len(string)):
+        sum_str += (ord(string[pos]) - 64) * (26**i)
+        pos -= 1
+    return sum_str
+
+def split_range(string):
+    mid = 0
+    for ch in string:
+        if ord(ch) >= 65:
+            mid += 1
+        else:
+            break
+    return string[:mid], string[mid:]
+
+def range_to_cell(string):
+    a, b = split_range(string)
+    return seq_to_int(a), int(b)
 
 def search_value(value, start, end):
-    start_a, start_b, end_a, end_b = "","","",""
     start_x, start_y, end_x, end_y = 0,0,0,0
-    count = 0
+    start_x, start_y = range_to_cell(start)
+    end_x, end_y = range_to_cell(end)
 
-    for ch in start:
-        if ord(ch) >= 65:
-            start_a += ch
-        else:
-            start_b += ch
-##
-##    for ch in end:
-##        if ord(ch) >= 65:   #ord('A') = 65
-##            end_x += ord(ch) - 64
-##        else:
-##            end_y += int(ch)
+    for i in range(end_x - start_x):
+        for j in range(end_y - end_x):
+            if Range((i,j)).value == value:
+                return (i,j)
+    return False
 
-##    return start_x,start_y,end_x,end_y
-    return start_a, start_b
 def main():
     pass
 
 if __name__ == '__main__':
-    print search_value(3,"AA1","Z10")
+    print search_value('E',"A1","K10")
