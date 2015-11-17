d5bb5c5d183acc0558b37bf765e9adc46cd7cd19
Lai99 <aaaxaaa99@gmail.com>
2015-11-17 21:21:12 +0800

    add data manage modify

--- data/data_manage.py
+++ data/data_manage.py
@@ -43,20 +43,23 @@ def _get_standard(table, data, start_pos, _, items_pos):
 def _get_channel(table, data, start_pos, _, items_pos):
     if "channel" in items_pos:
 ##############################################################################
-############## HT40 channel need to add "2" for real, HT80 channel need to add 2 or 6
+############## TX HT40 channel need to add "2" for real, TX HT80 channel need to add 2 or 6
         if "standard" in items_pos:
             s = table.row_values(start_pos)[items_pos["standard"]]
 
-        if "40" in s:
-            data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) + 2)
-
-        elif "80" in s:
-            if "120" in table.row_values(start_pos)[items_pos["channel"]]:
+        if not "rx_result" in items_pos:
+            if "40" in s:
                 data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) + 2)
-            elif "157" in table.row_values(start_pos)[items_pos["channel"]]:
-                data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) - 2)
+
+            elif "80" in s:
+                if "120" in table.row_values(start_pos)[items_pos["channel"]]:
+                    data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) + 2)
+                elif "157" in table.row_values(start_pos)[items_pos["channel"]]:
+                    data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) - 2)
+                else:
+                    data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) + 6)
             else:
-                data["channel"] = str(int(table.row_values(start_pos)[items_pos["channel"]]) + 6)
+                data["channel"] = table.row_values(start_pos)[items_pos["channel"]]
 ##############################################################################
         else:
             data["channel"] = table.row_values(start_pos)[items_pos["channel"]]
